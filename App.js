85c58d5dba2c5bcc4b95133a3ce0097c36d427c4
rselvaku <Password1>
2017-11-03 14:53:56 +0000

    committing file for calculating

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactCalculator from './src/ReactCalculator'
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <ReactCalculator/>
+    );
+  }
+}
